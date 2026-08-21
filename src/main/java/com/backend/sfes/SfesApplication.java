34a39f339a8f3525fc36ef900cbdc0cd4d5fc9e0
Jessa Gozun <jessanazarenedietagozun@gmail.com>
2026-08-21 16:37:22 +0800

    initial commit

---
+++ src/main/java/com/backend/sfes/SfesApplication.java
@@ -0,0 +1,13 @@
+package com.backend.sfes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/sfes/SfesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.sfes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
