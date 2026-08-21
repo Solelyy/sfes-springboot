e2ece5a98af1491fdc2d16b37bba0e1d7356d924
Jessa Gozun <jessanazarenedietagozun@gmail.com>
2026-08-21 18:43:05 +0800

    add common

---
+++ src/main/java/com/sfes/common/classes/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.sfes.common.classes;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    private void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    private void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
