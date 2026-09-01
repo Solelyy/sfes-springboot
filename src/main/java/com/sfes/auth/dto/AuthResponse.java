002cd3fc52bc42fa4993b5abc081f71efb31e63d
Jessa G <jessag@Jessas-MacBook-Pro.local>
2026-09-01 23:03:34 +0800

    change to record class

--- src/main/java/com/sfes/auth/dto/AuthResponse.java
+++ src/main/java/com/sfes/auth/dto/AuthResponse.java
@@ -1,17 +1,13 @@
 package com.sfes.auth.dto;
 
 import com.sfes.user.enums.Role;
-import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class AuthResponse {
-    private Long id;
-    private Role role;
-    private String email;
-    private String message;
+public record AuthResponse(
+        String email,
+        Role role,
+        String message
+) {
+    public AuthResponse(String message) {
+        this(null, null, message);
+    }
 }
-
