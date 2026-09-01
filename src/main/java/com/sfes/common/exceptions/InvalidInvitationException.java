60dc5a7715bd4f72b7307988fcbc9cadbbaf38ff
Jessa G <jessag@Jessas-MacBook-Pro.local>
2026-09-02 01:37:19 +0800

    for invalid account invitation

---
+++ src/main/java/com/sfes/common/exceptions/InvalidInvitationException.java
@@ -0,0 +1,7 @@
+package com.sfes.common.exceptions;
+
+public class InvalidInvitationException extends RuntimeException {
+    public InvalidInvitationException(String message) {
+        super(message);
+    }
+}
