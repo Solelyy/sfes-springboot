118d9cddc50e7c48169556a40ab42211ed87dbd5
Jessa G <jessag@Jessas-MacBook-Pro.local>
2026-09-01 23:02:51 +0800

    remove api prefix + refactor

--- src/main/java/com/sfes/auth/AuthController.java
+++ src/main/java/com/sfes/auth/AuthController.java
@@ -1,73 +1,57 @@
 package com.sfes.auth;
 
 import com.sfes.auth.dto.AuthResponse;
+import com.sfes.auth.dto.AuthResult;
 import com.sfes.auth.dto.LoginRequest;
 import com.sfes.auth.service.AuthService;
 import com.sfes.auth.service.CookieService;
-import com.sfes.security.jwt.JwtService;
-import com.sfes.user.entity.User;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 public class AuthController {
+
     private final AuthService authService;
-    private final JwtService jwtService;
     private final CookieService cookieService;
 
-    @Value("${cookie.secure}")
-    private boolean cookieSecure;
-
-    @Value("${cookie.sameSite}")
-    private String sameSite;
-
-    //login
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
-        User user = authService.authenticateUser(
-                loginRequest.getEmail(),
-                loginRequest.getPassword()
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
+        AuthResult result = authService.login(
+                loginRequest.email(),
+                loginRequest.password()
         );
 
-        String email = user.getEmail();
-        String token = jwtService.generateToken(email);
+        ResponseCookie cookie =
+                cookieService.createCookie(result.token());
 
-        ResponseCookie cookie = cookieService.createCookie(token);
-
-        AuthResponse authResponse = AuthResponse.builder()
-                .id(user.getId())
-                .email(email)
-                .role(user.getRole())
-                .message("Login successful")
-                .build();
+        AuthResponse response = new AuthResponse(
+                result.user().getEmail(),
+                result.user().getRole(),
+                "Login successful"
+        );
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, cookie.toString())
-                .body(authResponse);
+                .body(response);
     }
 
-    //logout
     @PostMapping("/logout")
     public ResponseEntity<AuthResponse> logout() {
-       ResponseCookie cookie = cookieService.removeCookie();
 
-        AuthResponse authResponse = AuthResponse.builder()
-                .message("Logout successfully")
-                .build();
+        ResponseCookie cookie = cookieService.removeCookie();
 
-        SecurityContextHolder.clearContext();
+        AuthResponse response = new AuthResponse(
+                "Logout successful"
+        );
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, cookie.toString())
-                .body(authResponse);
+                .body(response);
     }
 }
-
