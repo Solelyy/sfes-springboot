79d7f1d6bdb973493dc0aea96f9ed93aced210a4
Solelyy <jessanazarenedietagozun@gmail.com>
2026-09-02 20:27:18 +0800

    feature: update account status

--- src/main/java/com/sfes/superadmin/management/AccountService.java
+++ src/main/java/com/sfes/superadmin/management/AccountService.java
@@ -1,8 +1,13 @@
 package com.sfes.superadmin.management;
 
+import com.sfes.common.exceptions.AccountNotFoundException;
+import com.sfes.common.exceptions.InvalidRequestException;
 import com.sfes.employee.entity.Employee;
 import com.sfes.employee.repository.EmployeeRepository;
+import com.sfes.user.entity.User;
 import com.sfes.user.enums.Role;
+import com.sfes.user.enums.Status;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -37,4 +42,27 @@ public class AccountService {
         return new AccountsResponse(accounts);
     }
 
+    @Transactional
+    public void updateAccountStatus(String employeeId, Status newStatus) {
+        Employee employee = employeeRepository.findByEmployeeId(employeeId)
+                .orElseThrow(() -> new AccountNotFoundException("Employee do not exists"));
+
+        User user = employee.getUser();
+        Status currentStatus = user.getStatus();
+
+        if (currentStatus == newStatus) {
+            throw new InvalidRequestException(
+                    "Change status request cannot be the same as the current status."
+            );
+        }
+
+        if(!currentStatus.canTransitionTo(newStatus)) {
+            throw new InvalidRequestException(
+                    "Cannot change account status from %s to %s"
+                            .formatted(currentStatus, newStatus)
+            );
+        }
+
+        user.setStatus(newStatus);
+    }
 }
