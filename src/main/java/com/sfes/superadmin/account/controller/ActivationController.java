306fbe5b9a3ad17ab499996facc576dfc7abaf45
Solelyy <jessanazarenedietagozun@gmail.com>
2026-09-04 02:30:14 +0800

    move sending email in here

--- src/main/java/com/sfes/superadmin/account/controller/ActivationController.java
+++ src/main/java/com/sfes/superadmin/account/controller/ActivationController.java
@@ -1,14 +1,13 @@
 package com.sfes.superadmin.account.controller;
 
-import com.sfes.common.classes.ApiResponse;
 import com.sfes.superadmin.account.AccountInvitation;
 import com.sfes.superadmin.account.dto.ActivationRequest;
+import com.sfes.superadmin.account.dto.ActivationResponse;
 import com.sfes.superadmin.account.dto.VerifyInvitationResponse;
 import com.sfes.superadmin.account.service.ActivationService;
 import com.sfes.auth.dto.AuthResponse;
-import com.sfes.auth.dto.AuthResult;
 import com.sfes.auth.service.CookieService;
-import com.sfes.common.classes.ApiMessage;
+import com.sfes.superadmin.account.service.SuccessfulActivationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -22,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 public class ActivationController {
     private final ActivationService activationService;
     private final CookieService cookieService;
+    private final SuccessfulActivationService successfulActivationService;
 
     @GetMapping("/{token}")
     public VerifyInvitationResponse verifyInvitation(@PathVariable String token) {
@@ -35,17 +35,19 @@ public class ActivationController {
 
     @PostMapping("/{token}/activation")
     public ResponseEntity<AuthResponse> activateAccount(@PathVariable String token, @Valid @RequestBody ActivationRequest request) {
-        AuthResult activationResult = activationService.activateAccount(
+        ActivationResponse result = activationService.activateAccount(
                 token, request.password(), request.confirmPassword()
         );
 
-        ResponseCookie cookie = cookieService.createCookie(activationResult.token());
+        ResponseCookie cookie = cookieService.createCookie(result.jwtToken());
 
         AuthResponse authResponse = new AuthResponse(
-                activationResult.user().getEmail(),
-                activationResult.user().getRole(),
+                result.email(),
+                result.role(),
                 "Account activated successfully."
         );
+
+        successfulActivationService.sendSuccessActivation(result.email(), result.firstName());
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, cookie.toString())
