f3c913ef27b7282be1cd2ca64df1881b16033560
Solelyy <jessanazarenedietagozun@gmail.com>
2026-09-04 21:29:01 +0800

    exception for expired token

---
+++ src/main/java/com/sfes/common/exceptions/ExpiredTokenException.java
@@ -0,0 +1,7 @@
+package com.sfes.common.exceptions;
+
+public class ExpiredTokenException extends RuntimeException {
+    public ExpiredTokenException(String message) {
+        super(message);
+    }
+}
