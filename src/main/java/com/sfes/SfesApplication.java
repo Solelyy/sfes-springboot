65fbab3d436c1773f6474983835c93bc4d58271f
Solelyy <jessanazarenedietagozun@gmail.com>
2026-09-11 20:52:23 +0800

    enable configuration properties scanning

--- src/main/java/com/sfes/SfesApplication.java
+++ src/main/java/com/sfes/SfesApplication.java
@@ -2,10 +2,12 @@ package com.sfes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SfesApplication {
 
 	static void main(String[] args) {
