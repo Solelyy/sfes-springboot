e92433375c7c431ed5310caffbd94d8ccb21eb45
Solelyy <jessanazarenedietagozun@gmail.com>
2026-09-13 17:06:12 +0800

    add resend invitation controller

---
+++ src/main/java/com/sfes/superadmin/account/controller/ResendInvitationController.java
@@ -0,0 +1,31 @@
+package com.sfes.superadmin.account.controller;
+
+import com.sfes.common.classes.ApiMessage;
+import com.sfes.superadmin.account.dto.ResendInvitation;
+import com.sfes.superadmin.account.dto.ResendInvitationResponse;
+import com.sfes.superadmin.account.service.EmailActivationService;
+import com.sfes.superadmin.account.service.ResendInvitationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/superadmin")
+@RequiredArgsConstructor
+public class ResendInvitationController {
+    private final EmailActivationService emailActivationService;
+    private final ResendInvitationService resendInvitationService;
+
+    @PostMapping("/resend-invitation")
+    public ApiMessage resendInvitation(@Valid @RequestBody ResendInvitation request) {
+        ResendInvitationResponse result =
+                resendInvitationService.resendInvitation(request.employeeId());
+
+        emailActivationService.sendActivationEmail(result.email(), result.firstName(), result.rawToken());
+
+        return new ApiMessage("Invitation resent successfully");
+    }
+}
