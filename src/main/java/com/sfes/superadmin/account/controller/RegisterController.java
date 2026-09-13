f9f250b55d92076b41f9e07d353768288b02486b
Solelyy <jessanazarenedietagozun@gmail.com>
2026-09-13 15:45:18 +0800

    remove excess slash

--- src/main/java/com/sfes/superadmin/account/controller/RegisterController.java
+++ src/main/java/com/sfes/superadmin/account/controller/RegisterController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/superadmin/")
+@RequestMapping("/superadmin")
 @RequiredArgsConstructor
 public class RegisterController {
     private final RegisterService registerService;
