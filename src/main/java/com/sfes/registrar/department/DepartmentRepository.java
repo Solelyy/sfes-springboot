7c47f98a65644981ec282fa891fbe80046b36838
Solelyy <jessanazarenedietagozun@gmail.com>
2026-09-25 13:02:23 +0800

    add method

--- src/main/java/com/sfes/registrar/department/DepartmentRepository.java
+++ src/main/java/com/sfes/registrar/department/DepartmentRepository.java
@@ -2,9 +2,13 @@ package com.sfes.registrar.department;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
     Optional<Department> findByDepartmentName(String departmentName);
     Optional<Department> findByDepartmentCode(String departmentCode);
+
+    List<Department> findByDepartmentCodeIn(Collection<String> departmentCode);
 }
