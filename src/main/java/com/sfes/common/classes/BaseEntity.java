982a4e621987fc69f6eee411081544a528c90748
Solelyy <jessanazarenedietagozun@gmail.com>
2026-09-25 12:57:56 +0800

    remove Id generation

--- src/main/java/com/sfes/common/classes/BaseEntity.java
+++ src/main/java/com/sfes/common/classes/BaseEntity.java
@@ -11,10 +11,6 @@ import java.time.Instant;
 
 @MappedSuperclass
 public abstract class BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     @Column(name = "created_at", nullable = false)
     private Instant createdAt;
 
